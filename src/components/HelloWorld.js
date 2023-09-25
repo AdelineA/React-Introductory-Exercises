85dc33d056847403b97b6b84cb6cfeac5c642060
Adeline Ayinkamiye <adelinemug6@gmail.com>
2023-09-25 15:51:06 +0200

    helloworld exercise

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HelloWorld() {
+  return (
+    <div className='bg-yellow-400 flex text-2xl font-semibold items-center justify-center p-96'>
+        <h4>Hello, World!</h4>
+    </div>
+  )
+}
