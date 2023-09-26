f9aff289ef15c045ec24a11211f1d783a07183b1
Adeline Ayinkamiye <adelinemug6@gmail.com>
2023-09-26 16:07:42 +0200

    removing bug

--- src/components/CreateList.js
+++ src/components/CreateList.js
@@ -3,10 +3,11 @@ import React from "react";
 const list = ["dog", "cat", "chicken", "cow", "sheep", "horse"];
 
 export default function CreateList() {
-  const listItems = list.map((item) => <li>{item}</li>);
+  const listItems = list.map((item) => <li>{item}</li>);  
   return (
     <div className="py-10 flex flex-col items-center justify-center">
       <ul className="list-disc">{listItems}</ul>
     </div>
   );
 }
+
